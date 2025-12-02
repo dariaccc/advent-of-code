bbd61dd0d24a6657beb80d1feb75710d6a3f0638
dasha <daria.carlberg@gmail.com>
2025-12-02 22:45:19 +0100

    day 2

--- day2.py
+++ day2.py
@@ -31,21 +31,33 @@ for i in range(len(input)):
 
     #print(rep)
 
-    half = ""
-    for j in range(rep):
-        half += (str(id)[j])
+    #half = ""
+    #for j in range(rep):
+     #   half += (str(id)[j])
 
-    inv = half + half
+    #inv = half + half
     #print("inv ", inv)
-    
+
     for id in range(lower_bound[i], upper_bound[i]):
-        if id == int(inv):
-            invalid_id.append(id)
-            invalid_id_sum += id
+
+        first_half = ""
+        second_half = ""
+
+        if (len(str(id)) % 2 == 0):
+            for x in range(0, rep):
+                first_half += str(id)[x]
+
+            for y in range(rep, 2*rep):
+                second_half += str(id)[y]
+            
+            #print(second_half)
+
+            if first_half == second_half:
+               # print("equal")
+                invalid_id.append(id)
+                invalid_id_sum += id
         id += 1
+#    print("next ", invalid_id)
 
 print(invalid_id)
 print(invalid_id_sum)
-
-#edge cases not dealth with - if the lower bound is even, that one needs to be accounted for invalid id, otherwise the upper bound
-#what happens for the lowest and highest numbers?
