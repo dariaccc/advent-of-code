d03f6d05eaaa5d41000c07a0248369da6c8c5532
dasha <daria.carlberg@gmail.com>
2025-12-03 22:25:47 +0100

    finished day 3

---
+++ Day 3/day3.2.py
@@ -0,0 +1,51 @@
+# find the largest number
+# find the largest number to the RIGHT of that number
+# put together as string
+
+with open("Day 3/input.txt", "r") as file:
+    bank = [line.strip() for line in file]
+
+print(bank)
+joltage = []
+
+for i in bank:
+    volt12 = []
+    search_list = []
+    stop = len(i) - 12
+
+    i = list(i)
+   # max_value = max(i)
+    #volt12.append(max_value)
+
+    index = 0
+    x = 1
+    for n in range(12)[::-1]:
+        
+        j = i[index:stop + 1:1]
+        print(j)
+       # print("stop: ", stop)
+
+        max_value = max(j)
+        #print(j.index(max(j)))
+        
+        print(max_value)
+        index = index + (j.index(max(j))) + 1 #does not work
+        print("index: ", index)
+
+        stop = len(i) - n
+        x += 1
+        volt12.append(max_value)
+    joltage.append(volt12)
+
+print(joltage)
+
+sums = 0
+for bank in joltage:
+    banks = ""
+    for a in bank:
+        banks += a
+    print(banks)
+    banks = int(banks)
+    sums += banks
+
+print(sums)
