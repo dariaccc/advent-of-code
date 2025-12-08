6e78839f0cfa1d6dc8b0d41891eea888d242ea8d
dasha <daria.carlberg@gmail.com>
2025-12-08 11:21:41 +0100

    finished day 7 part 1

---
+++ Day 7/main.py
@@ -0,0 +1,28 @@
+
+with open("Day 7/input.txt", "r") as file:
+    input = [line.strip() for line in file]
+
+previous = input[0]
+new_input = [previous]
+counter = 0
+
+for i in input[1::]:
+    i = list(i)
+    for idx, char in enumerate(previous):
+        if char == "S":
+            i[idx] = "|"
+        elif char == "^" and pre_previous[idx] != ".":
+            i[idx + 1] = "|"
+            i[idx - 1] = "|"
+            counter += 1
+        elif char == "|" and i[idx] != "^":
+            i[idx] = "|"
+    i = "".join(i)
+    new_input.append(i)
+    pre_previous = previous
+    previous = i
+
+for j in new_input:
+    print(j)
+
+print(counter)
